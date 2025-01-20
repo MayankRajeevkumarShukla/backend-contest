8948a7d8eb25811f309bff0f3c370f534832e8b0
MayankRajeevkumarShukla <97381684+MayankRajeevkumarShukla@users.noreply.github.com>
2025-01-20 20:02:54 +0530

    Creating the backend contest

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const contestRoutes = require('./routes/contestRoutes')
+const testCaseRoutes  = require('./routes/testCaseRoutes')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api/contest',contestRoutes)
+app.use('/api/testCase',testCaseRoutes )
+app.get('/',(req,res)=>{
+    res.send('Welcome to the Contest Backend API')
+})
+module.exports = app;
